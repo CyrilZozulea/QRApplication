186fa1a72d573162c7e89622f9fee50f4965a518
KZozulea <KZozulea@ZOZULEAK>
2022-05-23 12:59:24 +0300

    update application.

--- QRApplication/AppShell.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace QRApplication;
-
-public partial class AppShell : Shell
-{
-    public AppShell()
-    {
-        InitializeComponent();
-    }
-}
--- QRApplication/QRPage.xaml.cs
+++ QRApplication/QRPage.xaml.cs
@@ -4,11 +4,36 @@ namespace QRApplication;
 
 public partial class QRPage : ContentPage
 {
+    private double R = 0;
+    private double G = 0;
+    private double B = 0;
+
     public QRPage(string qr)
     {
         InitializeComponent();
 
         QR qrcode = new QR(qr, "M", 0);
         QRCodeBarcode.Text = qrcode.Encode();
+    }
+
+    void RSliderValue(object sender, ValueChangedEventArgs args)
+    {
+        R = args.NewValue;
+        rLabelValue.Text = args.NewValue.ToString("F3");
+        QRCodeBarcode.TextColor = Color.FromRgb(R, G, B);
+    }
+
+    void GSliderValue(object sender, ValueChangedEventArgs args)
+    {
+        G = args.NewValue;
+        gLabelValue.Text = args.NewValue.ToString("F3");
+        QRCodeBarcode.TextColor = Color.FromRgb(R, G, B);
+    }
+
+    void BSliderValue(object sender, ValueChangedEventArgs args)
+    {
+        B = args.NewValue;
+        bLabelValue.Text = args.NewValue.ToString("F3");
+        QRCodeBarcode.TextColor = Color.FromRgb(R, G, B);
     }
 }
